8459d3078f64ec667820c6d6d8c6024592d8a6b3
Manuel Werlberger <manuel.werlberger@dacuda.com>
2014-08-16 17:11:53 +0200

    added dummy cmake project

---
+++ memory_transfer/memory_transfer_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//--------------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+   std::cout << "We are alive!" << std::endl;
+   
+   return EXIT_SUCCESS;
+}
