7fd0ae7e1695f6fa9742bcb794f903ec1c195b25
Manuel Werlberger <manuel.werlberger@dacuda.com>
2014-08-16 17:43:57 +0200

    includes not all there yet

--- memory_transfer/memory_transfer_main.cpp
+++ memory_transfer/memory_transfer_main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <cuda_runtime.h>
 
 //--------------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
-   std::cout << "We are alive!" << std::endl;
-   
-   return EXIT_SUCCESS;
+  std::cout << "We are alive!" << std::endl;
+
+//  // set device
+//  cudaDeviceProp device_prop;
+//  int dev_id = findCudaDevice(argc, (const char **) argv);
+//  checkCudaErrors(cudaGetDeviceProperties(&device_prop, dev_id));
+//  std::cout << "set device id: " << dev_id << std::endl;
+
+
+
+  cudaDeviceReset();
+  return EXIT_SUCCESS;
 }
