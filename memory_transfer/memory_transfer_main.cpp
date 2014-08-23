1c24e887532d8e2c2b711f25dcc715ad60037ee8
Manuel Werlberger <manuel.werlberger@dacuda.com>
2014-08-23 22:36:20 +0200

    added include to the sdk helper files

--- memory_transfer/memory_transfer_main.cpp
+++ memory_transfer/memory_transfer_main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <cuda_runtime.h>
+#include <helper_cuda.h>
 
 //--------------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
   std::cout << "We are alive!" << std::endl;
 
-//  // set device
-//  cudaDeviceProp device_prop;
-//  int dev_id = findCudaDevice(argc, (const char **) argv);
-//  checkCudaErrors(cudaGetDeviceProperties(&device_prop, dev_id));
-//  std::cout << "set device id: " << dev_id << std::endl;
+  // set device
+  cudaDeviceProp device_prop;
+  int dev_id = findCudaDevice(argc, (const char **) argv);
+  checkCudaErrors(cudaGetDeviceProperties(&device_prop, dev_id));
+  std::cout << "set cuda device id: " << dev_id << std::endl;
 
 
 
