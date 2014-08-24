1d65eeda965f21c56bfab5dd4b9c7a393386d205
Manuel Werlberger <manuel.werlberger@dacuda.com>
2014-08-24 22:30:53 +0200

    minor

--- memory_transfer/memory_transfer_main.cpp
+++ memory_transfer/memory_transfer_main.cpp
@@ -2,6 +2,9 @@
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 //--------------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
@@ -13,8 +16,15 @@ int main(int argc, char** argv)
   checkCudaErrors(cudaGetDeviceProperties(&device_prop, dev_id));
   std::cout << "set cuda device id: " << dev_id << std::endl;
 
+  cv::Mat im1 = cv::imread("/home/mwerlberger/data/std/Growth_of_cubic_bacteria_25x16.jpg");
+  std::cout << "im1 size: " << im1.cols << "x" << im1.rows << std::endl;
 
+  // copy im1 to global memory
+  uchar* d_im1;
+  const size_t size = im1.rows * im1.cols * sizeof(float);
 
-  cudaDeviceReset();
+//  cv::imshow("input", im1);
+//  //cv::imshow("result", im1);
+//  cv::waitKey();
   return EXIT_SUCCESS;
 }
